a9f9a25acabb252430869a1fd377b3f177578c21
tlevi101 <z4tnsz@inf.elte.hu>
2022-07-28 20:38:28 +0200

    my-secrets route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { MySecretsComponent } from './my-secrets/my-secrets.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'my-secrets', component: MySecretsComponent}
 ];
 
 @NgModule({
