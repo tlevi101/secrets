910ed39db76e400582af309b6ecfa24c0754236f
tlevi101 <z4tnsz@inf.elte.hu>
2022-08-01 14:07:15 +0200

    share form

--- src/app/share/share.component.ts
+++ src/app/share/share.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-share',
   templateUrl: './share.component.html',
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShareComponent implements OnInit {
 
-  constructor() { }
+  public myName="ShareComponent";
+  shareForm: FormGroup;
+  showShareBTN=true;
+  showLoading = false;
+  authorized =false;
+  @Output() onShareSubmit = new EventEmitter<{viewLimit: number, ttl:any}>();
+  constructor() { 
+    const FB= new FormBuilder();
+    this.shareForm = FB.group({
+      viewLimit:['',[
+        Validators.required,
+        Validators.pattern('^[0-9]+$'),
+      ]],
+      ttl:['',
+      ]
+    });
+    this.TTL?.valueChanges.subscribe(() => {
+      let hoursInMS= 5*60*60*1000;
+      console.log(new Date(this.TTL?.value));
+      console.log((new Date().getTime()+hoursInMS));
+      console.log((new Date(this.TTL?.value).getTime())< (new Date().getTime()+hoursInMS));
 
+      if((new Date(this.TTL?.value).getTime())< (new Date().getTime()+hoursInMS)) {
+        this.TTL?.setErrors({tooEarly:true})
+        console.log(this.TTL);
+      }
+      else{
+        this.TTL?.setErrors(null);
+      }
+    });
+  }
+  share(){
+    this.onShareSubmit.emit({viewLimit: this.ViewLimit?.value, ttl: this.TTL?.value});
+  }
   ngOnInit(): void {
   }
-
+  get ViewLimit(){
+    return this.shareForm.get('viewLimit');
+  }
+  get TTL(){
+    return this.shareForm.get('ttl');
+  }
 }
